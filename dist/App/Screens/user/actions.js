290cc0cbda1da9a8485728f8b54c726c7876111a
kbittner15 <61249059+kbittner15@users.noreply.github.com>
2021-10-05 17:59:45 -0400

    trying

--- dist/App/Screens/user/actions.js
+++ dist/App/Screens/user/actions.js
@@ -3,7 +3,8 @@ import { SET_CURRENT_USER } from './types';
 export const setCurrentUser = currentUser => ({
   type: SET_CURRENT_USER,
   payload: currentUser
-});
+}); //
+
 export const LogUserIn = () => async (dispatch, getState) => {
   try {
     const {
--- src/App/Screens/user/index.js
+++ src/App/Screens/user/index.js
@@ -1,4 +1,4 @@
 import * as actions from './actions'
 import reducers from './reducers'
 
-export { actions, reducers }
+export { actions, reducers }//
